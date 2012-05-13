5aab9b83b5b1ea3875ecc861564e4f9c8a8b2f3a
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-05-13 16:34:34 +0200

    lower/upper case

--- install-emacs24.sh
+++ install-emacs24.sh
@@ -160,6 +160,15 @@ tee "$EM_DIR"/init.el <<EOF
      ;; 'unfindable' on big projects
      (setq ffip-limit 2048)))
 
+;; ===================================================================
+;;                    lower/upper case
+;; ===================================================================
+
+;; C-x C-l
+(put 'downcase-region 'disabled nil)
+;; C-x C-u
+(put 'upcase-region 'disabled nil)
+
 EOF
 
 
